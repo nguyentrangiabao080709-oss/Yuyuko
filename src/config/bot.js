cccb9c4153d28c8926203ca7382ad9416e1bce05
nguyentrangiabao080709-oss <nguyentrangiabao080709@gmail.com>
2026-06-07 23:35:02 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#FF69B4", 
+      secondary: "#00BCE3", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
