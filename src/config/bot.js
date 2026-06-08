2d753a2dd1f0851a5009e98510f7178243b7da9f
nguyentrangiabao080709-oss <nguyentrangiabao080709@gmail.com>
2026-06-09 01:23:02 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "The Ghost House",
+        name: "Yuyu's Snacks",
         // Activity type number (0 = Playing).
-        type: 1, 
+        type: 5, 
       },
     ],
   },
